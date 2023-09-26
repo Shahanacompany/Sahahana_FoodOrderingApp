c091615aed65a5e7a6fcd9a897a4121ff4f0377d
Shahanacompany <146055312+Shahanacompany@users.noreply.github.com>
2023-09-26 22:55:10 +0530

    background color

--- Component/AboutScreen.js
+++ Component/AboutScreen.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { Button, View, Text } from "react-native";
 export default function AboutScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center",backgroundColor:"#ADD8E6"}}>
       <Text>About Screen</Text>
     </View>
   );
--- Component/HomeScreen.js
+++ Component/HomeScreen.js
@@ -3,7 +3,7 @@ import { Button, View, Text } from "react-native";
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor:"#ADD8E6" }}>
       <Text>Home Screen</Text>
       <Button
         title="Go to About"
